5624126a818c5453bcf0add63340889e1a3d4914
yedaodao <404069912@qq.com>
2016-12-10 02:20:45 +0800

    add basic logic

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 var posix = require('./lib/posix');
 
-function delTail() {
-
-}
-
-function delHead() {
-
-}
-
-function addTail() {
-
-}
-
-function addHead() {
-
-}
-
-function resolveParams() {
-
-}
-
 module.exports = {
     join: posix.join,
     addHeadSlash: posix.addHeadSlash,
-    addTailSlash: posix.addTailSlash
+    deleteHeadSlash: posix.deleteHeadSlash,
+    addTailSlash: posix.addTailSlash,
+    deleteTailSlash: posix.deleteTailSlash
 };

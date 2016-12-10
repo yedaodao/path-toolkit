9547d98cae7f0c0fe94674b82f40df6a128aeccb
yedaodao <404069912@qq.com>
2016-12-10 11:39:42 +0800

    fix typo of syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-var path = require('path');
+var path = require('path'),
+    webpack = require('webpack');
 
 module.exports = {
     entry: {
         "path-tookit": ['./index.js']
     },
     output: {
-        path: path.resolve(__dirname, "build/"),
+        path: path.resolve(__dirname, "dist/"),
         filename: "[name].js",
         libraryTarget: 'umd'
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('comment demo'),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
